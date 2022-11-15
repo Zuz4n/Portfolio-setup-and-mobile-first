3df2a911dac004b11a016e73fefb05062ce31a82
Zuz4n <113370956+Zuz4n@users.noreply.github.com>
2022-11-15 14:44:30 -0600

    Add objects in js file to popups

--- navScript.js
+++ navScript.js
@@ -14,3 +14,38 @@ function myFunction() {
       }
     }
   }
+
+  function functionPopup() {
+    var popup = document.getElementById("b1");
+    popup.classList.toggle("show");
+  }
+  
+  let popupProj1 = [
+  
+    popupMultipopMobile {
+      cross_icon: image.src="./images/Iconx.png",
+      title: "Multi Post Stories",
+      square_html: "html",
+      squate_boot: "bootstrap",
+      square_ruby: "ruby",
+      image: image.src="./images/numberten.png",
+      textmulti: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,",
+      btn_seelive: "See Live",
+      btn_seeSource: "See Source",
+    }
+  
+    popupMultipopMobile2 {
+      cross_icon: image.src="./images/Iconx.png",
+      title: "Multi Post Stories",
+      square_html: "html",
+      squate_boot: "bootstrap",
+      square_ruby: "ruby",
+      image: image.src="./images/numberten.png",
+      textmulti: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,",
+      btn_seelive: "See Live",
+      btn_seeSource: "See Source",
+    };
+  
+  ]
+  
+  
