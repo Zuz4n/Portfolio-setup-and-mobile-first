d0b14752b14a77b6b7912d128b96c896d715e262
diego <dgcuenca@gmail.com>
2022-11-15 16:16:21 -0500

    Create popup container

--- navScript.js
+++ navScript.js
@@ -16,36 +16,27 @@ function myFunction() {
   }
 
   function functionPopup() {
-    var popup = document.getElementById("b1");
-    popup.classList.toggle("show");
+    displayLayoutpopup();
   }
   
   let popupProj1 = [
-  
-    popupMultipopMobile {
-      cross_icon: image.src="./images/Iconx.png",
+    {
+      cross_icon: "./images/Iconx.png",
       title: "Multi Post Stories",
       square_html: "html",
       squate_boot: "bootstrap",
       square_ruby: "ruby",
-      image: image.src="./images/numberten.png",
+      image: "./images/numberten.png",
       textmulti: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,",
       btn_seelive: "See Live",
       btn_seeSource: "See Source",
     }
-  
-    popupMultipopMobile2 {
-      cross_icon: image.src="./images/Iconx.png",
-      title: "Multi Post Stories",
-      square_html: "html",
-      squate_boot: "bootstrap",
-      square_ruby: "ruby",
-      image: image.src="./images/numberten.png",
-      textmulti: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,",
-      btn_seelive: "See Live",
-      btn_seeSource: "See Source",
-    };
-  
   ]
-  
+
+  function displayLayoutpopup() {
+    let containerPopup = document.createElement('div');
+    containerPopup.innerHTML= `<h2>${popupProj1[0].title}</h2>`
+
+
+  } 
   
