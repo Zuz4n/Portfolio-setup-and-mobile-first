883196c962e3b8a4de881ac7b1e0cdafecba784a
Zuz4n <113370956+Zuz4n@users.noreply.github.com>
2022-11-17 18:43:57 -0600

    Update function to preserve data

--- preserve.js
+++ preserve.js
@@ -1,43 +1,44 @@
-
-var fullname = document.getElementById('name');
+var name = document.getElementById('name');
 var email = document.getElementById('email');
 var text = document.getElementById('textarea');
+var form = document.getElementById('signup');
 
+const formData = {
+userName: '',
+userEmail: '',
+text: '',
+}
 
-if(!Storage.length) {
-  populateStorage();
-  console.log('Enter to populateStorage');
-} else {
-  console.log('else');
+function populateStorage() {
+  formData.userName=form.userName.value;
+  formData.userEmail=form.userEmail.value;
+  formData.text=form.text.value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+  
   setStyles();
 }
+
+console.log(localStorage);
+
+function setStyles() {
+  if (localStorage.getItem("formData")===null){
+    formData.userName='';
+    formData.userEmail='';
+    formData.text='';
+
+  }else{
+    let data=JSON.parse(localStorage.getItem("formData"));
+    formData.userName=data.userName;
+    formData.userEmail=data.userEmail;
+    formData.text=data.text;
+    form.userName.value = formData.userName;
+    form.userEmail.value = formData.userEmail;
+    form.text.value = formData.text;
+  }
+  }
+ 
+  setStyles();
 
 fullname.onchange = populateStorage;
 email.onchange = populateStorage;
 text.onchange = populateStorage;
-
-
-function populateStorage() {
-  localStorage.setItem('name', document.getElementById('name').value);
-  localStorage.setItem('email', document.getElementById('email').value);
-  localStorage.setItem('textarea', document.getElementById('textarea').value);
-  
-  console.log(localStorage);
-
-  
-  setStyles();
-}
-
-function setStyles() {
-  var currentname = localStorage.getItem('name');
-  var currentmail = localStorage.getItem('email');
-  var currentext = localStorage.getItem('textarea');
-
-
-  document.getElementById ('name').innerHTML = currentname;
-
-  document.getElementById('email').innerHTML = currentmail;
-  console.log(document.getElementById('name').value);
-  document.getElementById('textarea').innerHTML = currentext;
-
-}
